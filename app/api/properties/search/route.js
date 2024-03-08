1b734459debe1e983527aaea9fade1d1d40f6dbd
Ameet Jayawant <ameetj85@gmail.com>
2024-03-08 11:07:24 -0500

    Property Search complete

---
+++ app/api/properties/search/route.js
@@ -0,0 +1,56 @@
+import connectDB from '@/config/database';
+import Property from '@/models/Property';
+
+// GET /api/properties/search
+export const GET = async (req) => {
+  try {
+    await connectDB();
+
+    let location = '';
+
+    const { searchParams } = new URL(req.url);
+    const tempLocation = searchParams.get('location');
+    const propertyType = searchParams.get('propertyType');
+
+    // if location is not returned correctly,
+    // i.e. iyt contans the url, then strip out everything
+    // until and including ?
+    if (tempLocation.includes('?')) {
+      location = tempLocation.split('?')[1].split('=')[1];
+      // console.log('location:', location);
+    } else {
+      location = tempLocation;
+    }
+
+    const locationPattern = new RegExp(location, 'i');
+
+    // match location pattern against db fields
+    let query = {
+      $or: [
+        { name: locationPattern },
+        { description: locationPattern },
+        { 'location.street': locationPattern },
+        { 'location.city': locationPattern },
+        { 'location.state': locationPattern },
+        { 'location.zipcode': locationPattern },
+      ],
+    };
+
+    // only check for property type if it is not All
+    if (propertyType && propertyType !== 'All') {
+      const typePattern = new RegExp(propertyType, 'i');
+      query.type = typePattern;
+    }
+
+    // console.log(query);
+
+    const properties = await Property.find(query);
+
+    // console.log(properties);
+
+    return new Response(JSON.stringify(properties, { status: 200 }));
+  } catch (error) {
+    console.log(error);
+    return new Response('Error', { status: 500 });
+  }
+};
--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 import PropertyCard from '@/components/PropertyCard';
+import PropertySearchForm from '@/components/PropertySearchForm';
 
 import { fetchProperties } from '@/utils/requests';
 
@@ -11,19 +12,27 @@ const PropertiesPage = async () => {
   properties.sort((a, b) => new Date(b.createdAt - new Date(a.createdAt)));
 
   return (
-    <section className='px-4 py-6'>
-      <div className='container-xl lg:container m-auto px-4 py-6'>
-        {properties.length === 0 ? (
-          <p>No properties found</p>
-        ) : (
-          <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-            {properties.map((prop) => (
-              <PropertyCard key={prop._id} property={prop} />
-            ))}
-          </div>
-        )}
-      </div>
-    </section>
+    <>
+      <section className='bg-blue-700 py-4'>
+        <div className='max-w-7xl mx-auto px-4 flex flex-col items-start sm:px-6 lg:px-8'>
+          <PropertySearchForm />
+        </div>
+      </section>
+
+      <section className='px-4 py-6'>
+        <div className='container-xl lg:container m-auto px-4 py-6'>
+          {properties.length === 0 ? (
+            <p>No properties found</p>
+          ) : (
+            <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+              {properties.map((prop) => (
+                <PropertyCard key={prop._id} property={prop} />
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
+    </>
   );
 };
 
